da22ad137a9b788173e890f73af82756e7e20d36
铁汉柔情 <3331247124@qq.com>
2025-07-25 14:31:55 +0800

    测试一下取模

---
+++ src/main/java/com/test/num/baifenbi.java
@@ -0,0 +1,11 @@
+package com.test.num;
+
+import javax.swing.*;
+
+public class baifenbi {
+    public static void main(String[] args) {
+        int i=46;
+        int j=23;
+        System.out.println(i%j);
+    }
+}
