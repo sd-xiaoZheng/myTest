0d866849de0517565cd1415a24f96e342a4340aa
铁汉柔情 <3331247124@qq.com>
2025-09-18 23:03:57 +0800

    试一下long能不能放小数点哈哈

--- src/main/java/com/test/num/xiaoshudian.java
+++ src/main/java/com/test/num/xiaoshudian.java
@@ -6,6 +6,7 @@ public class xiaoshudian {
     public static void main(String[] args) {
         double a=0.2255;
         float b=0.3f;
+        long c=0L;
         System.out.println(1d-a);
     }
 }
